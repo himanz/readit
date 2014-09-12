fc875adb980fcd1ed340a541abcea70b7761ade6
Jonathan Chang <jonoschang7@gmail.com>
2014-09-11 21:36:46 -0400

    add create new comments feature

--- app.js
+++ app.js
@@ -65,5 +65,13 @@ angular.module('flapperNews', ['ui.router'])
 '$stateParams',
 'posts',
 function ($scope, $stateParams, posts){
+	$scope.addComment = function(){
+		if ($scope.body === '') { return; }
+		$scope.post.comments.push({
+      body: $scope.body,
+      author: 'user',
+      upvotes: 0
+		}); 
+	};
 }]);
 
